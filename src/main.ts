f0605bd1bf925460fa22c943e36ca4476cbff8cf
MaryAnzh <maryang@mail.ru>
2026-04-03 20:47:05 +0300

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule } from '@nestjs/swagger';
+import * as YAML from 'yamljs';
+import { join } from 'path';
+
+const port = process.env.PORT ?? 4000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,10 +18,14 @@ async function bootstrap() {
     }),
   );
 
-  const port = 4000;
+  const document = YAML.load(join(process.cwd(), 'src/doc/api.yaml'));
+
+  SwaggerModule.setup('/doc', app, document);
+
   await app.listen(port);
 
   console.log(`🚀 Server is running on http://localhost:${port}`);
+  console.log(`📘 Swagger documentation is available at http://localhost:${port}/doc`);
 }
 
 bootstrap();
